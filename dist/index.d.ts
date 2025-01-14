7a9306d004cfcce41c69d89402dd3f08e442c894
Steve Montgomery <steve@chums.com>
2025-01-14 12:25:08 -0700

    fix: Add BasicAlert, ErrorAlert types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,3 +3,4 @@ export type { ResponseHandler } from './fetch.js';
 export { default as LocalStore } from './local-store.js';
 export { default as SessionStore } from './session-store.js';
 export { commaFormatter } from './utils.js';
+export type { BasicAlert, ErrorAlert } from './alerts.js';
---
+++ src/alerts.ts
@@ -0,0 +1,13 @@
+export interface BasicAlert {
+    title?: string,
+    message?: string,
+    context?: string,
+    canDismiss?: boolean,
+}
+
+export interface ErrorAlert {
+    id: number;
+    context: string;
+    message: string;
+    count: number;
+}
