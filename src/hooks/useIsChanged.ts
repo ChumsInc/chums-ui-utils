78fc5ab9bebedf390dc3c8ee9df301c0351989af
Steve Montgomery <steve@chums.com>
2025-07-21 11:48:40 -0600

    fix: add a wait option for useIsChanged in order to debounce state change.

--- src/hooks/useIsChanged.ts
+++ src/hooks/useIsChanged.ts
@@ -1,11 +1,17 @@
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import isEqual from "react-fast-compare";
 
-export default function useIsChanged<T = unknown>(value:T, compareTo:T) {
+export default function useIsChanged<T = unknown>(value:T, compareTo:T, wait?:number) {
     const [changed, setChanged] = useState(false);
+    const timer = useRef<number>(0)
     useEffect(() => {
-        const same = isEqual(value, compareTo);
-        setChanged(!same);
-    }, [value, compareTo]);
+        timer.current = window.setTimeout(() => {
+            const same = isEqual(value, compareTo);
+            setChanged(!same);
+        }, wait ?? 350);
+        return () => {
+            window.clearTimeout(timer.current);
+        }
+    }, [value, compareTo, wait]);
     return changed;
 }
